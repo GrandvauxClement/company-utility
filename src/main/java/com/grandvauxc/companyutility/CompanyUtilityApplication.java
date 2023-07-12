7c15a95a18c7f3d3d48b95133638ab31a8a5b4a0
clement Grandvaux <clement.grandvaux@hotmail.com>
2023-07-12 21:41:47 +0200

    first commit

---
+++ src/main/java/com/grandvauxc/companyutility/CompanyUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.grandvauxc.companyutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyUtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyUtilityApplication.class, args);
+    }
+
+}
