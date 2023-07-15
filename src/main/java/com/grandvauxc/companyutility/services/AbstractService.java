15ea9ff1e71c915246538a86b9edaae53d14d326
Simon Grandvaux <clement.grandvaux@hotmail.com>
2023-07-15 19:48:47 +0200

    create abstract service

--- src/main/java/com/grandvauxc/companyutility/services/AbstractService.java
+++ src/main/java/com/grandvauxc/companyutility/services/AbstractService.java
@@ -1,32 +1,48 @@
 package com.grandvauxc.companyutility.services;
 
+import com.grandvauxc.companyutility.exceptions.EntityNotFoundException;
+import com.grandvauxc.companyutility.repository.CompanyUtilityRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 import java.util.List;
 
-public class AbstractService implements AbstractServiceInterface{
+@Service
+public class AbstractService<T, ID extends Serializable, R extends CompanyUtilityRepository<T, ID>> implements AbstractServiceInterface<T,ID>{
 
+    private final R repository;
+
+    AbstractService() {
+        this.repository = null;
+    }
+    public AbstractService(R repository) {
+        this.repository = repository;
+    }
     @Override
-    public Object create(Object entity) {
-        return null;
+    public T create(T entity) {
+        return repository.save(entity);
     }
 
     @Override
-    public Object findById(Object idEntity) {
-        return null;
+    public T findById(ID idEntity) {
+        return repository.findById(idEntity).orElse(null);
     }
 
     @Override
-    public List findAll() {
-        return null;
+    public List<T> findAll() {
+        return repository.findAll();
     }
 
     @Override
-    public void updateById(Object o, Object entity) {
-
+    public void updateById(ID id, T entity) {
+        if (!repository.existsById(id)) throw new EntityNotFoundException((Class<?>) entity, id);
+        repository.save(entity);
     }
 
     @Override
-    public void deleteById(Object o, Object entity) {
-
+    public void deleteById(ID id, T entity) {
+        if (!repository.existsById(id)) throw new EntityNotFoundException((Class<?>) entity, id);
+        repository.deleteById(id);
     }
 
     @Override
